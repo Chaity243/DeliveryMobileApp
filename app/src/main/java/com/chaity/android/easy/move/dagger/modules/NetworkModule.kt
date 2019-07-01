eb64dcac8916dc2fd11e70a9ee279d762a232fae
Chaitanya Aggarwal <chaitanya.aggarwal@nagarro.com>
2019-07-01 07:33:08 +0530

    updated

--- app/src/main/java/com/chaity/android/easy/move/dagger/modules/NetworkModule.kt
+++ app/src/main/java/com/chaity/android/easy/move/dagger/modules/NetworkModule.kt
@@ -6,35 +6,18 @@ import com.chaity.android.easy.move.api.DeliveryAPI
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 class NetworkModule {
-
-
     @Singleton
     @Provides
-    internal fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor {
-        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
-    }
-
-
-
-    @Singleton
-    @Provides
-    internal fun providesClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
-        return OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+    internal fun providesClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
 
     }
-
-
-
-
-
-
 
     @Singleton
     @Provides
